05a6acb27aefa862dff5385fea68d825a1324148
LittleChest <hiya@littlew.top>
2025-07-22 17:23:11 +0800

    common: Update Anti-GFW * Adapt to new changes in GFW

--- common.js
+++ common.js
@@ -9,9 +9,14 @@ export default async function handler(
 
   // Anti-GFW
   if (
-    headers.get("user-agent") === "Go-http-client/1.1" &&
+    method === "POST" &&
     pathname === "/dns-query" &&
-    method === "POST"
+    headers.get("content-type") === "application/dns-message" &&
+    headers.get("accept") === "application/dns-message" &&
+    headers.get("accept-encoding") === "gzip, br" &&
+    headers.get("content-length") === "29" &&
+    (headers.get("user-agent") === "Go-http-client/1.1" ||
+      headers.get("user-agent") === "Go-http-client/2.0")
   ) {
     return new Response(null, { status: 403 });
   }
